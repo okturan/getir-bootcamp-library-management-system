f87c4664bcdfd125595b100fd230ddac448107fb
okturan <okanert@gmail.com>
2025-04-29 03:08:20 +0300

    Initial commit

---
+++ src/main/java/com/okturan/getirbootcamplibrarymanagementsystem/GetirBootcampLibraryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.okturan.getirbootcamplibrarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetirBootcampLibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetirBootcampLibraryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/okturan/getirbootcamplibrarymanagementsystem/GetirBootcampLibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okturan.getirbootcamplibrarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetirBootcampLibraryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
